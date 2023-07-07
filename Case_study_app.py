a6aa0434b5f7374c1779600e6daa6c59a80466e3
martynacieszy <66182147+martynacieszy@users.noreply.github.com>
2023-07-07 17:57:23 +0200

    Update Case_study_app.py

--- Case_study_app.py
+++ Case_study_app.py
@@ -93,7 +93,7 @@ def average_price(sales_NY, airbnb_NY, borough_name, room_type, room_type2, room
 import streamlit as st
 from streamlit_jupyter import StreamlitPatcher, tqdm
 StreamlitPatcher().jupyter() 
-tab1, tab2, tab3 = st.tabs(["Graphs","Areas comparison", "Map"])
+tab1, tab2 = st.tabs(["Graphs","Areas comparison"])
   
 boroughs = sales_NY["Borough"].unique()
 
@@ -191,7 +191,3 @@ with tab2:
         st.write("Choose neighborhoods for comparison in the sidebar.")
     else:
         st.write(areas_df)
-
-
-with tab3:
-    st.map(data=airbnb_NY, zoom=None, use_container_width=True)
