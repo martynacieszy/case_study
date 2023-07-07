27263499995ee9aec4f2960dfd3be441dd7b0ce7
martynacieszy <66182147+martynacieszy@users.noreply.github.com>
2023-07-07 09:13:12 +0200

    Update Case_study_app.py

--- Case_study_app.py
+++ Case_study_app.py
@@ -21,7 +21,7 @@ def sales_figure(sales_NY):
     mean_sales['Mean price per square ft'] = mean_sales['Mean price per square ft'].round(2)
     sales_fig = px.pie(mean_sales, values = "Mean price per square ft", names = mean_sales.index, 
                  title = "Average price per square feet in apartment in each district of New York")
-    sales_figure.update_traces(textinfo='value')
+    sales_figure.update_traces(textinfo='values')
     return sales_fig
 
 def mean_rent_func(airbnb_NY):
@@ -33,7 +33,7 @@ def mean_rent_func(airbnb_NY):
     mean_rent['Mean rent rate'] = mean_rent['Mean rent rate'].round(2)
     rent_fig = px.pie(mean_rent, values = "Mean rent rate", names = mean_rent.index, 
                          title = "Average rent price per " + str.lower(room_type) + " in each district of New York")
-    rent_fig.update_traces(textinfo='value')
+    rent_fig.update_traces(textinfo='values')
     
     return rent_fig, mean_rent
 
@@ -71,7 +71,7 @@ def average_price(sales_NY, airbnb_NY, borough_name, room_type, room_type2, room
     fig = px.bar(mean_sales_n, x = "Mean price per square ft", y = mean_sales_n.index, 
               title = "Average price per square feet in apartament and average price of renting of " + str.lower(room_type) + 
                  " in each neighborhood of " + str(borough_name))
-
+    fig.update_traces(marker={"color":"magenta"})
     mean_rent_n = (airbnb_NY.loc[np.logical_and(airbnb_NY['Borough'] == borough_name, 
                                                 np.logical_or.reduce((airbnb_NY['Room Type'] == room_type,
                                                                      airbnb_NY['Room Type'] == room_type2,
