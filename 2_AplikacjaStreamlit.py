7ca3d9702016c12f9075ea798f35faeb03c4e1f7
@MartynaCieszynska <martyna.cieszynska@aize.io>
2024-11-30 15:06:57 +0100

    rename column

--- 2_AplikacjaStreamlit.py
+++ 2_AplikacjaStreamlit.py
@@ -279,7 +279,7 @@ with tab2:
                 "Price Per Square Ft": "Cena stopy kwadratowej",
                 "Price Per Rental": "Cena wynajmu",
                 "Price Per Square Ft/Price Per Rental": "Cena stopy kwadratowej/Cena wynajmu",
-                "Availability 365": "Średnia ilość dni wynajmu dostępnych w roku",
+                "Availability 365": "Dostępność dni wynajmu w roku",
             }
         )
     with st.container():
